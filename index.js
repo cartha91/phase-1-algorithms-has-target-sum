6f05a11e3f9788fa511011fa0511df30cb1d15f7
steven dulworth <crowscoupetech@proton.me>
2023-10-07 14:09:06 -0500

    further practice

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+// create function
+  // function should accept two arguments
+  // args = target integer and array of integers
+  // return true if any pair of number inside given array add up to given target
+  function hasTargetSum(array, target) {
+    let arraySet = new Set();
+    for (let num of array) {
+        if (arraySet.has(target - num)) {
+            return true;
+        }
+        arraySet.add(num);
+    }
+    return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  0(n)
 */
 
 /* 
-  Add your pseudocode here
+  check if any pair of numbers in given array add up to the sum of the target integer
+
+  if a matching pair is found return true
+
+  if a matching pair is not found return false 
+
 */
 
 /*
-  Add written explanation of your solution here
+  my function starts by itterating over an array and evaluating it to determine if the differnce between numbers is in the current set,
+   if there is a match found it will return a value of true, if it does not find a match it will return a value of false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  /*
+    describe('hasTargetSum', function() {
+    it('should return true if any pair of numbers in the array adds up to the target', function() {
+        let array = [1, 2, 3, 4, 5];
+        let target = 9;
+        expect(hasTargetSum(array, target)).to.be.true;
+    });
+
+    it('should return false if no pair of numbers in the array adds up to the target', function() {
+        let array = [1, 2, 3, 4, 5];
+        let target = 10;
+        expect(hasTargetSum(array, target)).to.be.false;
+    });
+});
+  */
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
